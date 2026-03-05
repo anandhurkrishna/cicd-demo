cf8603ba54474005c706a9e714a3c2d527c2d69b
Anandhu krishnan R <anandhukrishna675@gmail.com>
2026-03-05 17:42:00 +0530

    Added health endpoint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Deployment Successful"
+    return "App Running"
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy",
+        "timestamp": time.time()
+    }), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
-

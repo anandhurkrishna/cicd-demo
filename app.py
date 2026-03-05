52eed96e2ab2b9caa6e6642e9d02c811c8398191
Anandhu krishnan R <anandhukrishna675@gmail.com>
2026-03-05 17:56:11 +0530

    Added prometheus

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify
 import time
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+# ---- Metrics ----
+REQUEST_COUNT = Counter("app_requests_total", "Total App Requests")
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.inc()
+
+# ---- Main Route ----
 @app.route("/")
 def home():
     return "App Running"
 
+# ---- Health Endpoint ----
 @app.route("/health")
 def health():
     return jsonify({
@@ -14,5 +24,11 @@ def health():
         "timestamp": time.time()
     }), 200
 
+# ---- Metrics Endpoint ----
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+# ---- Run App ----
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
